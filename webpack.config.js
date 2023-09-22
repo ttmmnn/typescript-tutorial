f9785a0f2358fce54f5ed60c68e99637a8ad54e9
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-09-22 22:01:30 +0900

    update_webpac.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,24 @@ module.exports = {
   },
   output: {
     path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  mode: 'development', //modeはproductionモードとdevelopmentモードの2つある
+  resolve: {
+    extensitons: ['.ts', '.js'], //拡張子を指定してくれる
+  },
+  devServer: {
+    static: {
+      directory: `${__dirname}/dist`,
+    },
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, //拡張子が.tsのファイルをコンパイルする
+        lorder: 'ts-lorder',
+      },
+    ],
   },
 };
